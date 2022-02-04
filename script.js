dfb415bf6e90ef52d5abbbfa8566c0f519343aa4
vinidnt <vinicius.wasp@gmail.com>
2022-02-04 19:40:03 -0300

    Adicionado funcionalidade da encurtamento de URL através da API

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let form = document.querySelector(".shorten-form");
 let nav = document.querySelector(".nav");
 let menu = document.querySelector(".menu");
 let body = document.querySelector("body");
-let input = document.querySelector(".shorten-form__input");
+var input = document.querySelector(".shorten-form__input");
 let button = document.querySelector(".shorten-form__button");
 
 input.addEventListener('invalid', () => {
@@ -17,7 +17,7 @@ input.addEventListener('input', () => {
     input.style.border = "1px solid hsl(0, 0%, 75%)";
 })
 
-button.addEventListener('submit', addShortenedLink);
+form.addEventListener('submit', addShortenedLink);
 
 //Menu
 
@@ -43,50 +43,84 @@ function closeMenu() {
     menu.style.color = "var(--grayish-violet)"
 }
 
-function clearInput(){
+let divCount = 0;
+
+function clearInput() {
     document.querySelector(".shorten-form__input").value = "";
 }
-function addShortenedLink(){
-    let mainContainer = document.querySelector(".dynamic-url-shortened")
-    let container = document.createElement("div");
+function addShortenedLink() {   
 
-    let row = document.createElement("div");
-    row.classList.add("row");
+    //HTTP Request
+    var request = new XMLHttpRequest()
 
-    let unshortenedLinkContainer = document.createElement("p");
-    unshortenedLinkContainer.classList.add("unshortened-link");
-
-    let shortenedLinkContainer = document.createElement("p");
-    shortenedLinkContainer.classList.add("shortened-link");
-    
-    
-
-    let unshortenedLinkText = document.createTextNode(document.querySelector(".shorten-form__input").value);
-    let shortenedLinkText = document.createTextNode("htpps://placeholder.com")
-        
-    let copyButton = document.createElement("button");
-    
-    copyButton.textContent = "Copy!";
-    unshortenedLinkContainer.appendChild(unshortenedLinkText);
-    shortenedLinkContainer.appendChild(shortenedLinkText);
-    mainContainer.appendChild(container);
-    container.appendChild(unshortenedLinkContainer);
-    container.appendChild(row);
-    container.appendChild(shortenedLinkContainer);
-
-    
-    container.appendChild(copyButton);
+    // Open a new connection, using the GET request on the URL endpoint
+    request.open('GET', 'https://api.shrtco.de/v2/shorten?url=' + document.querySelector(".shorten-form__input").value, true)
 
 
+    request.onload = function () {
+        // Begin accessing JSON data here
+        var data = JSON.parse(this.response);
+        console.log(data.ok);
+        if (data.ok) {
 
-    //localStorage.setItem("div1", text)
-    clearInput();
+            if (divCount > 2) {
+                let elem = document.querySelector("div[id^=div]");
+                elem.remove();
+            }
+            
+            let mainContainer = document.querySelector(".dynamic-url-shortened")
+            let container = document.createElement("div");
+            container.setAttribute("id", `div${divCount}`);
+
+            let row = document.createElement("div");
+            row.classList.add("row");
+
+            let unshortenedLinkContainer = document.createElement("p");
+            unshortenedLinkContainer.classList.add("unshortened-link");
+            unshortenedLinkContainer.value = input.value;
+            var shortenedLinkText = document.createTextNode(data.result.short_link);
+            
+            let shortenedLinkContainer = document.createElement("p");
+            shortenedLinkContainer.classList.add("shortened-link");
+            shortenedLinkContainer.appendChild(shortenedLinkText);
+
+            let unshortenedLinkText = document.createTextNode(document.querySelector(".shorten-form__input").value);
+
+            let copyButton = document.createElement("button");            
+            copyButton.textContent = "Copy!";
+            copyButton.classList.add("unshortened-link");
+            copyButton.addEventListener("click", function() {
+                let div = copyButton.parentNode
+                let shortenedLink = div.querySelector(".shortened-link");
+                navigator.clipboard.writeText(shortenedLink.innerHTML);
+            });
+
+            unshortenedLinkContainer.appendChild(unshortenedLinkText);
+            mainContainer.appendChild(container);
+            container.appendChild(unshortenedLinkContainer);
+            container.appendChild(row);
+            container.appendChild(shortenedLinkContainer);
+            copyButton.addEventListener("click", copyToClipboard())
+            container.appendChild(copyButton);
+
+            divCount++;
+
+            clearInput();
+        } else {
+            var invalidMessage = input.nextElementSibling;
+            invalidMessage.style.display = "flex";
+            input.style.border = "3px solid var(--red)";
+        }
+    }
+    // Send request
+    request.send();
+
+
 }
-
-function setLocalStorageItens(){
-localStorage.getItem("div")
+function setLocalStorageItens() {
+    localStorage.getItem("div")
 }
-
-
-
-
+function copyToClipboard() {
+    let text = document.querySelector(".shortened-link").innerHTML;
+    navigator.clipboard.writeText(text);
+}
